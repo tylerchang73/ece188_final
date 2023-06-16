a16caad28146c77dc3acc11df0a5138ad92610dc
tylerchang73 <67492510+tylerchang73@users.noreply.github.com>
2023-06-16 14:45:32 -0700

    Initial commit

---
+++ mmdetection_changes/kitti/kitti_f2dn.py
@@ -0,0 +1,104 @@
+_base_ = [
+    '../_base_/datasets/kitti_dataset.py',
+    '../_base_/default_runtime.py'
+]
+# model settings
+model = dict(
+    type='CSP',
+    pretrained='open-mmlab://msra/hrnetv2_w32',
+    backbone=dict(
+        type='HRNet',
+        extra=dict(
+            stage1=dict(
+                num_modules=1,
+                num_branches=1,
+                block='BOTTLENECK',
+                num_blocks=(4,),
+                num_channels=(64,)),
+            stage2=dict(
+                num_modules=1,
+                num_branches=2,
+                block='BASIC',
+                num_blocks=(4, 4),
+                num_channels=(32, 64)),
+            stage3=dict(
+                num_modules=4,
+                num_branches=3,
+                block='BASIC',
+                num_blocks=(4, 4, 4),
+                num_channels=(32, 64, 128)),
+            stage4=dict(
+                num_modules=3,
+                num_branches=4,
+                block='BASIC',
+                num_blocks=(4, 4, 4, 4),
+                num_channels=(32, 64, 128, 256))
+        ),
+        # frozen_stages=-1,
+        norm_eval=False,
+    ),
+    neck=dict(
+        type='HRFPN',
+        in_channels=[32, 64, 128, 256],
+        out_channels=768,
+        num_outs=1),
+    bbox_head=dict(
+        type='CSPHead',
+        num_classes=2,
+        in_channels=768,
+        stacked_convs=1,
+        feat_channels=256,
+        strides=[4],
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=0.01),
+        loss_bbox=dict(type='IoULoss', loss_weight=0.05),
+        loss_offset=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=0.1)),
+    #refine_head=dict(
+    #    type='RefineHead',
+    #    num_cls_fcs=2,
+    #    num_cls_convs=2,
+    #    in_channels=768,
+    #    fc_out_channels=1024,
+    #    alpha=0.5,
+    #    dropout_prob=0.5,
+    #    weight_decay=0.0005,
+    #    roi_feat_size=7,
+    #    loss_cls=dict(
+    #        type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+    #    ),
+    )
+
+# training schedule, voc dataset is repeated 3 times, in
+# `_base_/datasets/voc0712.py`, so the actual epoch = 4 * 3 = 12
+max_epochs = 4
+train_cfg = dict(
+    type='EpochBasedTrainLoop', max_epochs=max_epochs, val_interval=1)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+# learning rate
+param_scheduler = [
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=max_epochs,
+        by_epoch=True,
+        milestones=[3],
+        gamma=0.1)
+]
+
+# optimizer
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001))
+
+# Default setting for scaling LR automatically
+#   - `enable` means enable scaling LR automatically
+#       or not by default.
+#   - `base_batch_size` = (8 GPUs) x (2 samples per GPU).
+auto_scale_lr = dict(enable=False, base_batch_size=16)
